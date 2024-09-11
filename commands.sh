c0782f3a9162f0245ab1a099526ec5091ee26895
Shasibhusan Jena <shasibhusan.dev@gmail.com>
2024-09-11 12:34:41 +0530

    added

---
+++ commands.sh
@@ -0,0 +1,9 @@
+# Create a S3 Bucket
+aws s3 mb s3://shjena-code-sam
+
+# package aws cloud formation
+aws cloudformation package --s3-bucket shjena-code-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
+
+# deploy
+# below commnand will create a change set
+aws cloudformation deploy --template-file C:\workspace_sample_project_AWS\SAMDeploymentUsngAWS\gen\template-generated.yaml --stack-name hello-world-sam-v0
---
+++ src/app.py
@@ -0,0 +1,5 @@
+## SAM FIle
+import json
+print("We are working using AWS SAM Package")
+def lambda_handler(event, context):
+    return "Hello world"
