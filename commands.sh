da3031192df9d68063f4188fb0c81050bfbf758f
Shasibhusan Jena <shasibhusan.dev@gmail.com>
2024-09-11 19:11:14 +0530

    added

--- commands.sh
+++ commands.sh
@@ -5,5 +5,5 @@ aws s3 mb s3://shjena-code-sam
 aws cloudformation package --s3-bucket shjena-code-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
 
 # deploy
-# below commnand will create a change set
-aws cloudformation deploy --template-file C:\workspace_sample_project_AWS\SAMDeploymentUsngAWS\gen\template-generated.yaml --stack-name hello-world-sam-v0
+# Below command will create a change set
+aws cloudformation deploy --template-file C:\workspace_sample_project_AWS\SAMDeploymentUsngAWS\gen\template-generated.yaml --stack-name hello-world-sam-v1 --capabilities CAPABILITY_IAM
